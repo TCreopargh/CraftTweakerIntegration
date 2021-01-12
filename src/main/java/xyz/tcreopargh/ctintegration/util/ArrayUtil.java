f092803b6de89d1ac00963b265411accecd50318
TCreopargh <TCreopargh@gmail.com>
2021-01-12 12:52:11 +0800

    array reverse

--- src/main/java/xyz/tcreopargh/ctintegration/util/ArrayUtil.java
+++ src/main/java/xyz/tcreopargh/ctintegration/util/ArrayUtil.java
@@ -6,6 +6,7 @@ import stanhebben.zenscript.annotations.ZenMethod;
 import xyz.tcreopargh.ctintegration.CTIntegrationMod;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @ZenRegister
 @ZenClass(CTIntegrationMod.CT_NAMESPACE + "util.ArrayUtil")
@@ -48,5 +49,45 @@ public class ArrayUtil {
     @ZenMethod
     public static void sort(char[] array) {
         Arrays.sort(array);
+    }
+
+    @ZenMethod
+    public static void reverse(int[] array) {
+        Collections.reverse(Arrays.asList(array));
+    }
+
+    @ZenMethod
+    public static void reverse(double[] array) {
+        Collections.reverse(Arrays.asList(array));
+    }
+
+    @ZenMethod
+    public static void reverse(float[] array) {
+        Collections.reverse(Arrays.asList(array));
+    }
+
+    @ZenMethod
+    public static void reverse(String[] array) {
+        Collections.reverse(Arrays.asList(array));
+    }
+
+    @ZenMethod
+    public static void reverse(short[] array) {
+        Collections.reverse(Arrays.asList(array));
+    }
+
+    @ZenMethod
+    public static void reverse(byte[] array) {
+        Collections.reverse(Arrays.asList(array));
+    }
+
+    @ZenMethod
+    public static void reverse(long[] array) {
+        Collections.reverse(Arrays.asList(array));
+    }
+
+    @ZenMethod
+    public static void reverse(char[] array) {
+        Collections.reverse(Arrays.asList(array));
     }
 }
